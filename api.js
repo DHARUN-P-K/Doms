8cb55d8f91fbdd57797412145195d6116bfb5570
DHARUN_P_K <dharunpk.23eie@kongu.edu>
2025-06-24 11:40:58 +0530

    after api

---
+++ api.js
@@ -0,0 +1,18 @@
+async function getActivity(){
+    const baseURL="https://bored-api.appbrewery.com/random/";
+    try{
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    console.log(data);
+    console.log(`Here's a fun activity:${data.activity}`);
+    
+}
+catch(error){
+    console.log(`some Error:${error}`);
+
+}
+finally{
+    console.log("Fetching completed");
+}
+}
+getActivity();
